0b8d535563c0196978cf91854fc9fdc76c1aecfa
Ankit <ankitthakur1998@gmail.com>
2022-04-25 08:24:50 +0530

    Comments added

--- src/com/luv2code/hibernate/demo/CreateCourseAndReviewsDemo.java
+++ src/com/luv2code/hibernate/demo/CreateCourseAndReviewsDemo.java
@@ -35,28 +35,11 @@ public class CreateCourseAndReviewsDemo {
 			//start a transaction
 			session.beginTransaction();
 						
-			//get the instructor from db
-			int theId = 1;
-			Instructor tempInstructor = session.get(Instructor.class, theId);
+			//create a course
 			
-			//Debugging Code
-			System.out.println("Value of tempInstructor" + tempInstructor);
+			//add some reviews
 			
-			//create some courses
-			Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
-			Course tempCourse2 = new Course("The Pinball Masterclass");
-						
-			//add courses to instructor
-			tempInstructor.add(tempCourse1);
-			tempInstructor.add(tempCourse2);
-			
-			//Debugging Code
-			System.out.println("Value of tempInstructor" + tempInstructor);
-			System.out.println("Value of tempInstructor" + tempInstructor);
-			
-			//save the courses
-			session.save(tempCourse1);
-			session.save(tempCourse2);
+			//save the course ... and leverage the cascade all
 			
 			//commit transaction
 			session.getTransaction().commit();
