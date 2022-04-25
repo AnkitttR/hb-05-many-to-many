ee88a2c826819bc6ba09d11a1ae7b5e7f21ae1d7
Ankit <ankitthakur1998@gmail.com>
2022-04-25 12:26:27 +0530

    Course.java is modified

--- src/com/luv2code/hibernate/demo/entity/Course.java
+++ src/com/luv2code/hibernate/demo/entity/Course.java
@@ -11,6 +11,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
@@ -37,14 +38,17 @@ public class Course {
 	@Column(name="title")
 	private String title;
 	
-	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE, 
-			CascadeType.DETACH, CascadeType.REFRESH})    //Many courses to one instructor
+	// Imp Note: Before "To" represents where we are, in this case (Course.java), and after "To" represents where we are pointing (One if we are pointing towards Instructor, Many if we are pointing towards Reviews.)
+	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})    //Many courses to one instructor
 	@JoinColumn(name="instructor_id")  //Course ka column hai instructor_id, jo point kar raha hai, towards primary key of instructor.
 	private Instructor instructor;  //Reference to Instructor class
 	
 	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL) //Because if we delete a course, then reviews must be deleted.
 	@JoinColumn(name="course_id")
 	private List<Review> reviews;
+	
+	@ManyToMany(fetch=FetchType.LAZY, cascade= {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+	private List<Student> students;
 	
 	public Course() {  //No argument constructor
 			
@@ -94,7 +98,26 @@ public class Course {
 			reviews=new ArrayList<>();
 		}
 		
-		reviews.add(theReview);  //theReview variable ki value , arraylist ke variable reviews mein add kar denge.
+		reviews.add(theReview);  //Hey reviews you have arraylist value, please add it to theReview variable.
+	}
+	
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+	
+	//add a convenience method
+	
+	public void addStudent(Student theStudent) {
+		if(students==null) {
+			students = new ArrayList<>();
+		}
+		
+		students.add(theStudent);  //Hey students you have arraylist value, please add it to theStudent variable.
 	}
 	
 	
